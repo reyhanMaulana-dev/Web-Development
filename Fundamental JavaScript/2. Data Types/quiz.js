419ddce7d410dac970401ab7cf59eb501ea70bc1
reyhanMaulana-dev <reyhanmaulanaaryaduta@gmail.com>
2024-08-15 09:06:28 +0700

    feat : quiz.js

---
+++ Fundamental JavaScript/2. Data Types/quiz.js
@@ -0,0 +1,13 @@
+const name = "Reyhan Maulana";
+const bornYear = 2004;
+let hobbby = "Olahraga";
+let addres = "Tangerang Selatan"
+
+let output;
+
+let currentYear = 2024;
+let age = currentYear - bornYear;
+
+output = `Hai nama saya ${name}, usia saya ${age} tahun, hobi saya ${hobbby}, saya tinggal di ${addres}.`;
+
+console.log(output);
