5b3a0d348303dbcf9e227493c9340afb1f264e47
reyhanMaulana-dev <reyhanmaulanaaryaduta@gmail.com>
2025-01-05 22:16:41 +0700

    feat : adding secondsary variant

--- Frontend/component/src/App.jsx
+++ Frontend/component/src/App.jsx
@@ -3,9 +3,13 @@ import Button from "./components/atoms/Button"
 function App() {
 
   return (
-    <div className="flex flex-col min-h-screen items-center px-12 py-8 gap-10">
+    <div className="flex flex-col min-h-screen items-center px-12 py-8 gap-5">
       <h1 className="font-bold text-4xl text-black">React Component</h1>
       <Button>BUTTON</Button>
+      <Button disabled>BUTTON</Button>
+      <Button variant="secondary">BUTTON</Button>
+      <Button variant="secondary" disabled>BUTTON</Button>
+
     </div>
   )
 }
